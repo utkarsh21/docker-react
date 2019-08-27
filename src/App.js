229000bb54fb2256a55bd0cb6a7ab3bfa58f4eb6
utkarsh21 <utk.set@gmail.com>
2019-08-27 09:22:34 +0000

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tested Docker using Nodejs!!!
+          Deployed to AWS ElasticBeanStalk!!!
         </p>
         <a
           className="App-link"
